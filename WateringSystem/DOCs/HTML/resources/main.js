3fe7afde90fba1f41cf97f7f1c11ef12f00cde68
zuglar <uglarz@gmail.com>
2021-12-23 02:07:43 +0100

    WS htm js css icons modified

--- WateringSystem/DOCs/HTML/resources/main.js
+++ WateringSystem/DOCs/HTML/resources/main.js
@@ -1,10 +1,13 @@
+/* Variables */
+var page = "index.htm"
+
 $(function () {
     /* Variables for main widow size and element size */
     var windowWidth = $(window).width();
-    var mainDivWidth = document.getElementById("main").offsetWidth;
+    var mainDivWidth = document.getElementById("container").offsetWidth;
 
     if (windowWidth > mainDivWidth) {
-        $("#main").css({
+        $("#container").css({
             left: ((windowWidth / 2) - (mainDivWidth / 2)) + "px"
         });
     }
@@ -14,18 +17,84 @@ $(function () {
     }
 
     var path = window.location.pathname;
-    var page = path.split("/").pop();
+    page = path.split("/").pop();
     console.log(page);
 
-    alert("--- " +window.location.pathname);
+    if (page == "" || page == "index.htm") {
+        valvesState();
+        wetnessRainSensorsState();
+    }
 });
-
+/* Load navbar from htm file */
 $(document).ready(function () {
     $('.navbar').load("./navbar.htm");
 });
-
-/* window.onload = function () {
-    if (document.querySelectorAll("section:target").length == 0) {
-        window.location = "#home";
+/* Function to show states of valves */
+function valvesState() {
+    var valves = document.getElementById("valves").textContent;
+    console.log("valves: " + valves);
+    if (valves.length != 8) {
+        alert("Warning!!!\nUnable to get data of state of valves!");
     }
-} */
+
+    var row = document.getElementById("valves-state");
+    for (var i = valves.length - 1; i >= 0; i--) {
+        var img = document.createElement('img');
+        var ttd = document.createElement("td");
+
+        if (valves[i] == "0") {
+            img.src = "./icns/green.png";
+        } else {
+            img.src = "./icns/red.png";
+        }
+        ttd.appendChild(img);
+        row.appendChild(ttd);
+    }
+}
+/* Function to show state of wetness of soil and sensors of rain */
+function wetnessRainSensorsState() {
+    var sensors = document.getElementById("wetness-rain").textContent;
+    console.log("sensors: " + sensors)
+    const valueArray = sensors.split(';', 10);
+    if (valueArray.length != 10) {
+        alert("ERROR!!!\nUnable to read wetness and rain sensors value!!!");
+        return;
+    }
+
+    var wetnessSensorNumber = 8;
+
+    var wetness = document.getElementById("wetness-state");
+    var rain = document.getElementById("rain-state");
+
+    for (let i = 0; i < valueArray.length; i++) {
+        var ttd = document.createElement("td");
+        /* ttd.setAttribute("class", "t8c_col"); */
+
+        if (i < wetnessSensorNumber) {
+            ttd.innerHTML = valueArray[i] + "%";
+            wetness.appendChild(ttd);
+        } else {
+            var img = document.createElement('img');
+            /* img.setAttribute("style", "margin-left: auto; margin-right: auto"); */
+            if (valueArray[i] == 0) {
+                img.src = "./icns/green.png";
+            } else {
+                img.src = "./icns/red.png";
+            }
+            ttd.appendChild(img);
+            rain.appendChild(ttd);
+        }
+    }
+}
+/* Refresh sensors data and valves statein every 2 seconds  */
+getData();
+setInterval(() => {
+    if (page == "index.htm") {
+        getData();
+    }
+}, 2000);
+
+function getData() {
+    var xhr = new XMLHttpRequest();
+    /* xhr.open("GET", "/update?output="+element.id+"&state=1", true); */
+}
