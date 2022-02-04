a5d56cec5c018ebd6bafcfdf6e2095a4846c85f0
zuglar <uglarz@gmail.com>
2022-02-04 15:35:33 +0100

    WS imac

--- WateringSystem/src/main.cpp
+++ WateringSystem/src/main.cpp
@@ -60,15 +60,15 @@ void loop() {
         return;
     }
 
-    if (asyncTcpWdt) {
-        printf("1. LOOP: asyncTcpWdt: %d\n", asyncTcpWdt);
-        /* controller->wifi32s->getClientData(); */
-        asyncTcpWdt = false;
-        delay(1);
-        printf("2. LOOP: asyncTcpWdt: %d\n", asyncTcpWdt);
-    }
-    delay(1);
-    controller->wifi32s->ftp->handle();
+    // if (asyncTcpWdt) {
+    //     printf("1. LOOP: asyncTcpWdt: %d\n", asyncTcpWdt);
+    //     /* controller->wifi32s->getClientData(); */
+    //     asyncTcpWdt = false;
+    //     delay(1);
+    //     printf("2. LOOP: asyncTcpWdt: %d\n", asyncTcpWdt);
+    // }
+    // delay(1);
+    // controller->wifi32s->ftp->handle();
 }
 
 bool startUp(void) {
